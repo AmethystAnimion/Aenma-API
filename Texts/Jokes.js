10e4be6fcab9c31703f6f8c57f8e4fe3d96aabde
Aenma Animion <36470027+AmethystAnimion@users.noreply.github.com>
2019-02-02 00:17:13 +0700

    Update Jokes.js

--- Texts/Jokes.js
+++ Texts/Jokes.js
@@ -25,19 +25,51 @@ const Jokes = [
       '`3.` Leave the toilet on 00:10, 1 January.',
       'Congrats! You\'ve spending a year on toilet!'
     ]
-  ],['ETC',
-   'Q) Why we write etc. at the end in the exam?\n\nA) Because, it means:\n\t**E**: End of\n\t**T**: Thinking\n\t**C**: Capacity'],
-  ['Breaking Up 1',
-   'Girlfriend: I am breaking up with you.\nBoyfriend: Okay.\nGirlfriend: You will not get another one like me anywhere.\nBoyfriend: Thank God.'],
-  ['Modern Way of Break Up',
-   'A boy throws 6 cricket balls at his girlfriend.\nWhen she asks, "What was that for?", The boy replies, "It\'s over."'],
-  ['Breaking Up 2', 'Boy: I wanna break up with you.\nGirl: You won\'t get anyone like me anywhere.\nBoy: I am not even going to search for someone like you.'],
-  ['My love to animals be like', 'Me, my wife and her mother are walking along the park. Suddenly I saw that a tiger attacks them. Whom will I save then?\n\nMe: I will save the tiger because they are going to be extinct.'],
-  ['Doing Time', 'Did you hear about the crook who stole a calendar?\nHe got twelve months.'],
-  ['Land and T', 'Q. Why is an island like the letter T?\n\nA. They\'re both in the middle of water!'],
-  ['When a hawk falls off a branch', 'Well, this is hawkward.'],
-  ['A whale is always right', 'Whale, whale, whale...look who was right again.'],
-  ['Copycats', 'Q: There were 10 cats in a boat and one jumped out. How many were left?\n\nA: None, because they were all a bunch of copycats.']
+  ],['ETC', [
+      'Q) Why we write etc. at the end in the exam?',
+      'A) Because, it means:',
+      '\t**E**: End of',
+      '\t**T**: Thinking',
+      '\t**C**: Capacity'
+    ]
+  ],['Breaking Up 1', [
+      'Girlfriend: I am breaking up with you.',
+      'Boyfriend: Okay.',
+      'Girlfriend: You will not get another one like me anywhere.',
+      'Boyfriend: Thank God.'
+    ]
+  ],['Modern Way of Break Up', [
+      'A boy throws 6 cricket balls at his girlfriend.',
+      'When she asks, "What was that for?", The boy replies, "It\'s over."'
+    ]
+  ],['Breaking Up 2', [
+      'Boy: I wanna break up with you.',
+      'Girl: You won\'t get anyone like me anywhere.',
+      'Boy: I am not even going to search for someone like you.'
+    ]
+  ],['My love to animals be like', [
+      'Me, my wife and her mother are walking along the park. Suddenly I saw that a tiger attacks them. Whom will I save then?',
+      'Me: I will save the tiger because they are going to be extinct.'
+    ]
+  ],['Doing Time', [
+      'Did you hear about the crook who stole a calendar?',
+      'He got twelve months.'
+    ]
+  ],['Land and T', [
+      'Q. Why is an island like the letter T?',
+      'A. They\'re both in the middle of water!'
+    ]
+  ],['When a hawk falls off a branch', [
+      'Well, this is hawkward.'
+    ]
+  ],['A whale is always right', [
+      'Whale, whale, whale...look who was right again.'
+    ]
+  ],['Copycats', [
+      'Q: There were 10 cats in a boat and one jumped out. How many were left?',
+      'A: None, because they were all a bunch of copycats.'
+    ]
+  ]
 ];
 
 module.exports = Jokes;
