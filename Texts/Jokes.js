1317833a1b75222934f04a4606da45db617c700b
Aenma Animion <36470027+AmethystAnimion@users.noreply.github.com>
2019-02-02 00:01:06 +0700

    Update Jokes.js

--- Texts/Jokes.js
+++ Texts/Jokes.js
@@ -1,6 +1,27 @@
 const Jokes = [
-  ['title', 'text'],
-  ['title', 'text']
+  ['Shiny Diamond',
+   'Student: I want to shine like a diamond!\nAlbert Einstein: Diamonds do not shine, they reflect, idiot.'],
+  ['Great Wall of China',
+   'Student: Does that great wall of China really visible from space?\nAstronaut: Only their recycling factories are visible from empty space.'],
+  ['The Chemical Formula',
+   'A teacher said to a student, "Go to the board and write the chemical formula of Water."\n\nThe student writes "HIJKLMNO".\n\nThe teacher tells him that he is wrong.\nThe student replies, "But Madam, you told us that it was H to O!'],
+  ['Spelling Error',
+   'A teacher said to a student, "Go to the board and show me how you spell `CROCODILE`."\n\nThe student writes "KROKODIAL".\n\nThe teacher tells him that he is wrong.\nThe student replies, "Madam, it maybe wrong, but you told me to show you how I spell it".'],
+  ['How to Poop in Toilet for A Year',
+   '`1.` Enter the toilet on 23:59, 31 Desember.\n`2.` Poop.\n`3.` Leave the toilet on 00:10, 1 January.\n\nCongrats! You\'ve spending a year on toilet!'],
+  ['ETC',
+   'Q) Why we write etc. at the end in the exam?\n\nA) Because, it means:\n\t**E**: End of\n\t**T**: Thinking\n\t**C**: Capacity'],
+  ['Breaking Up 1',
+   'Girlfriend: I am breaking up with you.\nBoyfriend: Okay.\nGirlfriend: You will not get another one like me anywhere.\nBoyfriend: Thank God.'],
+  ['Modern Way of Break Up',
+   'A boy throws 6 cricket balls at his girlfriend.\nWhen she asks, "What was that for?", The boy replies, "It\'s over."'],
+  ['Breaking Up 2', 'Boy: I wanna break up with you.\nGirl: You won\'t get anyone like me anywhere.\nBoy: I am not even going to search for someone like you.'],
+  ['My love to animals be like', 'Me, my wife and her mother are walking along the park. Suddenly I saw that a tiger attacks them. Whom will I save then?\n\nMe: I will save the tiger because they are going to be extinct.'],
+  ['Doing Time', 'Did you hear about the crook who stole a calendar?\nHe got twelve months.'],
+  ['Land and T', 'Q. Why is an island like the letter T?\n\nA. They\'re both in the middle of water!'],
+  ['When a hawk falls off a branch', 'Well, this is hawkward.'],
+  ['A whale is always right', 'Whale, whale, whale...look who was right again.'],
+  ['Copycats', 'Q: There were 10 cats in a boat and one jumped out. How many were left?\n\nA: None, because they were all a bunch of copycats.']
 ];
 
 module.exports = Jokes;
